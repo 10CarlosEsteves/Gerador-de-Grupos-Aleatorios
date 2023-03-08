bf3b3f8a69d6c536923f85a8468c65c18ef158e4
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2023-03-07 21:01:45 -0300

    Create GeradorAleatorio.py

---
+++ GeradorAleatorio.py
@@ -0,0 +1,23 @@
+import Rian
+import random
+
+listaDeNomes=[]
+listaAleatoria=[]
+qtdDeGrupos = int(0)
+
+ArquivoDeBusca = "veteranos.csv"
+qtdDeGrupos = 4
+
+listaDeNomes =  Rian.ListaComNomes(ArquivoDeBusca, listaDeNomes)
+qtdDeMembros = len(listaDeNomes)
+
+#Exibindo os nomes dos partipantes do escritório
+print("Lista de Nomes de participantes do Escritório:")
+for nome in listaDeNomes:
+    print(f"-{nome}")
+print(f"Tamanho total da lista: {qtdDeMembros}\n\n")
+
+listaAleatoria = Rian.GeradorAleatorio(listaDeNomes, 4)
+
+print(listaAleatoria)
+
