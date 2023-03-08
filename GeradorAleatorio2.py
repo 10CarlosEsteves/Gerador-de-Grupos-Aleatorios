8ab020294e651907f251747ee9ec4cb90e3f4eae
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2023-03-08 16:47:28 -0300

    Update GeradorAleatorio2.py

--- GeradorAleatorio2.py
+++ GeradorAleatorio2.py
@@ -1,16 +1,16 @@
-import Rian
+import GeradorDeGrupos
 
-listaDeNomes=[]
-listaAleatoria=[]
-ArquivoDeBusca = str()
-qtdDeGrupos = int(0)
+nomes=[]
+gruposEmbaralhados=[]
+arquivoDeBusca = ""
+qtdDeGrupos = 0
 
 print("\n\n")
 print("------------------------------------------------------------------------")
-ArquivoDeBusca = input("Por favor, diga o nome (ou o caminho) do arquivo .csv: ")
+arquivoDeBusca = input("Por favor, diga o nome (ou o caminho) do arquivo .csv: ")
 qtdDeGrupos = int(input("Por favor, insira a quantidade de grupos que deseja formar: "))
 
-listaDeNomes = Rian.ListaComNomes(ArquivoDeBusca, listaDeNomes)
-listaAleatoria = Rian.GeradorAleatorio(listaDeNomes, qtdDeGrupos)
+nomes = GeradorDeGrupos.ExtrairNomes(arquivoDeBusca+".csv", nomes)
+grupoEmbaralhado = GeradorDeGrupos.GerarGrupos(nomes, qtdDeGrupos)
 
-Rian.PrintListaAleatoria(listaAleatoria, qtdDeGrupos)
+GeradorDeGrupos.MostrarListaAleatoria(grupoEmbaralhado)
