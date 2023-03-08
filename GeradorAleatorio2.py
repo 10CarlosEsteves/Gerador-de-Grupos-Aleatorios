e30d2842dd03c389762f83a2aa779bad95438510
Rian Ernesto <91086261+RianErnesto@users.noreply.github.com>
2023-03-08 14:48:47 -0300

    :buld: docs: Comentários de review de código adicionados em GeradorAleatorio2.py e Rian.py

--- GeradorAleatorio2.py
+++ GeradorAleatorio2.py
@@ -1,12 +1,20 @@
+# Use nomes descritivos nas suas libs, não consigo identificar o que isso faz pelo nome
 import Rian
 
+# Ao iniciar uma variável de lista, não precisa dizer que é uma lista, apenas use um nome descritivo e deixe indicado, ex.: nomes e nomesEmbaralhados;
+# Ao colocar no plural já vai estar indicado que é uma lista
 listaDeNomes=[]
 listaAleatoria=[]
+
+# Nome de variável inicia com letra minúscula
+# No Python, seja direto ao inicar a variável, inicie-a com seu valor padrão, ex.: nomeArquivo = "" e quantidadeGrupos = 0
 ArquivoDeBusca = str()
 qtdDeGrupos = int(0)
 
 print("\n\n")
 print("------------------------------------------------------------------------")
+
+# Não quero ter que colocar .csv no final do input, você já diz que vou inserir um arquivo do tipo .csv, então é desnecessário
 ArquivoDeBusca = input("Por favor, diga o nome (ou o caminho) do arquivo .csv: ")
 qtdDeGrupos = int(input("Por favor, insira a quantidade de grupos que deseja formar: "))
 
