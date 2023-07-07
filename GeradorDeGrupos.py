0461b7cdbf63e780c566cd42c167b8f80b75bec5
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2023-07-07 17:08:44 -0300

    Update GeradorDeGrupos.py

--- GeradorDeGrupos.py
+++ GeradorDeGrupos.py
@@ -1,61 +1,45 @@
-import csv
-import random
-
-def ExtrairNomes(nomeDoArquivo, listaACadastrar):
-    Arquivo = open(nomeDoArquivo, "r")
-    arquivo_csv = csv.reader(Arquivo, delimiter=",")
-    for lista in arquivo_csv:
-        for nome in lista:
-            listaACadastrar.append(nome)
-    Arquivo.close()
-    return listaACadastrar
-
-def GerarGrupos(lista, numGrupos):
-    divisaoExata = len(lista) // numGrupos
-    matriz = []
-
-    # criando matriz
-    for i in range(numGrupos):
-        matriz.append([])
-
-    # distribuindo "pessoas exatas" entre os grupos
-    if(len(lista)%numGrupos==0):
-        for nome in lista:
-            while True:
-                aleatorio = int(random.randint(0, numGrupos-1))
-
-                if len(matriz[aleatorio]) < divisaoExata:
-                    matriz[aleatorio].append(nome)
-                    break
-
-    # distribuindo "pessoas não exatas" entre os grupos
-    else:
-        limite = numGrupos*(len(lista)//numGrupos)
-        for pos in range(0, limite):
-            while True:
-                aleatorio = int(random.randint(0, numGrupos-1))
-
-                if len(matriz[aleatorio]) < divisaoExata:
-                    matriz[aleatorio].append(lista[pos])
-                    break
-        del(lista[0:limite])
-
-        for nome in lista:
-            while True:
-                aleatorio = int(random.randint(0, numGrupos-1))
-
-                if len(matriz[aleatorio]) < divisaoExata+1:
-                    matriz[aleatorio].append(nome)
-                    break
-
-    return matriz
-
-def MostrarListaAleatoria(lista):
-    indice = int(1)
-    for grupo in lista:
-        print("--------------------------------")
-        print(f"PARTICIPANTES DO GRUPO {indice}")
-        for nome in grupo:
-            print(f"-{nome}")
-        indice+=1
+def extrair_nomes(nome_arquivo):
+    """Esta função extrai os nomes do arquivo .csv, armazena em uma lista e a retorna"""
+    with open(nome_arquivo, encoding="UTF-8") as arquivo_csv:
         
+        matriz = csv.reader(arquivo_csv, delimiter=",")
+
+        for lista in matriz:
+            nomes = [nome for nome in lista]
+    return nomes
+
+
+def gerar_grupos(nomes, num_grupos):
+    """Esta função gera uma matriz de grupos aleatórios."""
+    pessoas_por_grupo = len(nomes) // num_grupos
+    
+    grupos = []
+
+    # distribuindo as pessoas exatas entre os grupos
+    for indice in range(num_grupos):
+        
+        grupo = random.sample(nomes, pessoas_por_grupo)
+        grupos.append(grupo) 
+        
+        for pessoa in grupo:
+            nomes.remove(pessoa)
+
+    # distribuindo os remanescentes entre os grupos
+    while nomes:
+        limite_por_grupo = pessoas_por_grupo + 1
+        indice = random.randrange( num_grupos )
+
+        if len(grupos[indice]) < limite_por_grupo:
+            pessoa = nomes.pop()
+            grupos[indice].append(pessoa)
+
+    return grupos
+
+def mostrar_grupos(grupos):
+    """Esta função mostra os grupos e seus respectivos participantes"""
+    for indice, grupo in enumerate(grupos):
+        print("--------------------------------")
+        print(f"PARTICIPANTES DO GRUPO {indice + 1}")
+        for nome in grupo:
+            print(f"-{nome} ")
+        indice+=1
