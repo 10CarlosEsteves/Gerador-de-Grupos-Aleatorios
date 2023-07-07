a6e8e0bbd62a6f16ac33c9d64ce6b9ea0f18e64c
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2023-07-07 17:15:49 -0300

    Update GeradorAleatorio2.py

--- GeradorAleatorio2.py
+++ GeradorAleatorio2.py
@@ -1,16 +1,19 @@
+import os
 import GeradorDeGrupos
 
-nomes=[]
-gruposEmbaralhados=[]
-arquivoDeBusca = ""
-qtdDeGrupos = 0
+os.system("cls")
+
+nomes = []
+grupos_embaralhados = []
+arquivo = ""
+numero_grupos = 0
 
 print("\n\n")
 print("------------------------------------------------------------------------")
-arquivoDeBusca = input("Por favor, diga o nome (ou o caminho) do arquivo .csv: ")
-qtdDeGrupos = int(input("Por favor, insira a quantidade de grupos que deseja formar: "))
+arquivo = input("Por favor, diga o nome (ou o caminho) do arquivo .csv: ")
+numero_grupos = int(input("Por favor, insira a quantidade de grupos que deseja formar: "))
 
-nomes = GeradorDeGrupos.ExtrairNomes(arquivoDeBusca+".csv", nomes)
-grupoEmbaralhado = GeradorDeGrupos.GerarGrupos(nomes, qtdDeGrupos)
+nomes = GeradorDeGrupos.extrair_nomes(arquivo + ".csv")
+grupos_embaralhados = GeradorDeGrupos.gerar_grupos(nomes, numero_grupos)
 
-GeradorDeGrupos.MostrarListaAleatoria(grupoEmbaralhado)
+GeradorDeGrupos.mostrar_grupos(grupos_embaralhados)
